1005c8c119e3d101cbfa0d65b3bc1ae11f1c7418
Arthur Mariano <arthvm@proton.me>
2025-08-18 08:06:58 -0300

    refactor: add const for qrcodes table name

--- Migrations/20250522001650_UpdateQRCodeSchema.cs
+++ Migrations/20250522001650_UpdateQRCodeSchema.cs
@@ -7,20 +7,22 @@ namespace AutoInsightAPI.Migrations
     /// <inheritdoc />
     public partial class UpdateQRCodeSchema : Migration
     {
+        private const string QRCODE_TABLE = "QRCodes";
+        
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropForeignKey(
                 name: "FK_QRCodes_Yards_YardId",
-                table: "QRCodes");
+                table: QRCODE_TABLE);
 
             migrationBuilder.DropIndex(
                 name: "IX_QRCodes_YardId",
-                table: "QRCodes");
+                table: QRCODE_TABLE);
 
             migrationBuilder.DropColumn(
                 name: "YardId",
-                table: "QRCodes");
+                table: QRCODE_TABLE);
         }
 
         /// <inheritdoc />
@@ -28,19 +30,19 @@ namespace AutoInsightAPI.Migrations
         {
             migrationBuilder.AddColumn<string>(
                 name: "YardId",
-                table: "QRCodes",
+                table: QRCODE_TABLE,
                 type: "NVARCHAR2(450)",
                 nullable: false,
                 defaultValue: "");
 
             migrationBuilder.CreateIndex(
                 name: "IX_QRCodes_YardId",
-                table: "QRCodes",
+                table: QRCODE_TABLE,
                 column: "YardId");
 
             migrationBuilder.AddForeignKey(
                 name: "FK_QRCodes_Yards_YardId",
-                table: "QRCodes",
+                table: QRCODE_TABLE,
                 column: "YardId",
                 principalTable: "Yards",
                 principalColumn: "Id",
